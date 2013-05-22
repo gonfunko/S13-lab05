fe793b554c4d22143626a9da082e3a5b9a545fe6
Alex Molina <alexmolina@umail.ucsb.edu>
2013-05-22 10:42:14 -0700

    fixed snowman, done with step 8

--- src/edu/ucsb/cs56/S13/drawings/alexmolina/simple/PictureComponent.java
+++ src/edu/ucsb/cs56/S13/drawings/alexmolina/simple/PictureComponent.java
@@ -16,7 +16,7 @@ import java.awt.geom.GeneralPath; // combinations of lines and curves
    A component that draws a Picture by Phill Conrad
    
    @author Phill Conrad (original drawing)
-   @author YOUR NAME HERE (fixed the snowmans's head)
+   @author Alex Molina (fixed the snowmans's head)
    @version for UCSB CS56, S13
 
    
@@ -93,15 +93,22 @@ public class PictureComponent extends JComponent
              snowManCenterBottomY - bottomRadius * 2 - middleRadius,
              middleRadius
            );
-       g2.draw(snowManMiddleCircle);
+      g2.draw(snowManMiddleCircle);
 
        // @@@ ADD CODE HERE TO DRAW THE TOP CIRCLE
-       
+      Circle snowManTopCircle = 
+           new Circle
+           (
+             snowManCenterBottomX,
+             snowManCenterBottomY - bottomRadius * 2 - middleRadius * 2 - topRadius,
+             topRadius
+           );
+       g2.draw(snowManTopCircle);       
        
        // @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
        
        g2.drawString("A house and a snowman, by Phill Conrad", 20,20);
-       g2.drawString("Top of snowman added by YOUR NAME HERE", 20,40);
+       g2.drawString("Top of snowman added by Alex Molina", 20,40);
     }
 
         
