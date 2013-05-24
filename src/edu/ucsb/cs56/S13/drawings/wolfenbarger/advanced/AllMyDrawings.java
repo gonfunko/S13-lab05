5af65d04db092f5ea87515254e91ee67d2abf7e6
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2013-05-24 14:45:46 -0700

    Finished three drawings and comments.

--- src/edu/ucsb/cs56/S13/drawings/wolfenbarger/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/S13/drawings/wolfenbarger/advanced/AllMyDrawings.java
@@ -27,133 +27,106 @@ import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses 
+    /** Draw a picture with a few caterpillar heads
      */
 
     public static void drawPicture1(Graphics2D g2) {
 
-	Caterpillar c1 = new Caterpillar(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(c1);
-	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
+	Caterpillar c1 = new Caterpillar(50,350,70,85);
+	g2.setColor(Color.RED); g2.draw(c1);
 
 	Shape c2 = ShapeTransforms.scaledCopyOfLL(c1,0.5,0.5);
-	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(c2);
-	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
+	c2 = ShapeTransforms.translatedCopyOf(c2,250,0);
+	g2.setColor(Color.GREEN); g2.draw(c2);
+        
 	c2 = ShapeTransforms.scaledCopyOfLL(c2,4,4);
-	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
+	c2 = ShapeTransforms.translatedCopyOf(c2,170,0);
 	
-	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
+	g2.setColor(Color.pink); 
 	g2.draw(c2);
 	
-	// Draw two houses with Windows
-	
-	CaterpillarWithHat ch1 = new CaterpillarWithHat(50,350,40,75);
-	CaterpillarWithHat ch2 = new CaterpillarWithHat(200,350,200,100);
+	CaterpillarWithHat ch1 = new CaterpillarWithHat(50,150,40,75);
+	CaterpillarWithHat ch2 = new CaterpillarWithHat(200,150,200,100);
 	
 	g2.draw(ch1);
 	g2.setColor(new Color(0x8F00FF)); g2.draw(ch2);
 	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
 	g2.setStroke(orig);
-	g2.setColor(Color.BLACK); 
-	g2.drawString("A few caterpillars by Martin Wolfenbarger", 20,20);
+	g2.setColor(Color.black);
+	g2.drawString("A few caterpillar heads by Martin Wolfenbarger", 20,20);
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few caterpillar heads
      */
     public static void drawPicture2(Graphics2D g2) {
-
-	// Draw some coffee cups.
-	
+        
 	Caterpillar large = new Caterpillar(100,50,225,150);
-	Caterpillar smallCC = new Caterpillar(20,50,40,30);
+	Caterpillar small = new Caterpillar(20,50,40,30);
 	Caterpillar tallSkinny = new Caterpillar(20,150,20,40);
 	Caterpillar shortFat = new Caterpillar(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
-	g2.setColor(Color.GREEN);   g2.draw(smallCC);
+	g2.setColor(Color.GREEN);   g2.draw(small);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	Caterpillar c1 = new Caterpillar(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(c1);
+	Caterpillar c1 = new Caterpillar(200,230,50,55);
+	g2.setColor(Color.blue); g2.draw(c1);
 	
-	// Make a black house that's half the size, 
-	// and moved over 150 pixels in x direction
 	Shape c2 = ShapeTransforms.scaledCopyOfLL(c1,0.5,0.5);
-	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(c2);
+	c2 = ShapeTransforms.translatedCopyOf(c2,250,0);
+	g2.setColor(Color.gray); g2.draw(c2);
 	
-	// Here's a house that's 4x as big (2x the original)
-	// and moved over 150 more pixels to right.
 	c2 = ShapeTransforms.scaledCopyOfLL(c2,4,4);
-	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
+	c2 = ShapeTransforms.translatedCopyOf(c2,350,0);
 	
-	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
-	
-	// for hex colors, see (e.g.) http://en.wikipedia.org/wiki/List_of_colors
-	// #002FA7 is "International Klein Blue" according to Wikipedia
-	// In HTML we use #, but in Java (and C/C++) its 0x
 	
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
-	g2.setColor(new Color(0x002FA7)); 
+	g2.setColor(Color.YELLOW);
 	g2.draw(c2);
 	
-	// Draw two houses with Windows
-	
 	CaterpillarWithHat ch1 = new CaterpillarWithHat(50,350,40,75);
-	CaterpillarWithHat ch2 = new CaterpillarWithHat(200,350,200,100);
+	CaterpillarWithHat ch2 = new CaterpillarWithHat(370,150,350,150);
 	
 	g2.draw(ch1);
-	g2.setColor(new Color(0x8F00FF)); 
-
-	// Rotate the second house 45 degrees around its center.
-	Shape ch3 = ShapeTransforms.rotatedCopyOf(ch2, Math.PI/4.0);
+	g2.setColor(new Color(0x8F00FF));
+        
+	Shape ch3 = ShapeTransforms.rotatedCopyOf(ch2, Math.PI/2.0);
 
 	g2.draw(ch3);
 	
-	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
-	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of caterpillars with and without their hats on by Martin Wolfenbarger", 20,20);
+	g2.drawString("A bunch of caterpillar heads with and without their hats on by Martin Wolfenbarger", 20,20);
     }
   
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few caterpillar heads
      */
 
     public static void drawPicture3(Graphics2D g2) {
+        
 	
-	// label the drawing
-	
-	g2.drawString("A bunch of caterpillars by Martin Wolfenbarger", 20,20);
-
-	
-	// Draw some coffee cups.
-	
-       Caterpillar large = new Caterpillar(100,50,225,150);
-       CaterpillarWithHat small = new CaterpillarWithHat(20,50,40,30);
+	g2.drawString("Big caterpillar with caterpillar blush", 20,20);
+        
+        Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+        g2.setStroke(thick);
+       Caterpillar large = new Caterpillar(100,50,400,300);
+        CaterpillarWithHat small1 = new CaterpillarWithHat(120,200,40,30);
+        CaterpillarWithHat small2 = new CaterpillarWithHat(450,200,40,30);
        
-       g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(small);
+        Shape smallLeft = ShapeTransforms.rotatedCopyOf(small1, Math.PI/-4.0);
+        Shape smallRight = ShapeTransforms.rotatedCopyOf(small2, Math.PI/4.0);
+       
+       g2.setColor(Color.GREEN);     g2.draw(large);
+        g2.setColor(Color.RED);   g2.draw(smallLeft);
+        g2.setColor(Color.RED);   g2.draw(smallRight);
        
        
     }
