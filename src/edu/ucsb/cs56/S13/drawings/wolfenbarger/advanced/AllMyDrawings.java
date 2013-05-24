de516ca2078fc9da1df25911ea7c937a44f363ba
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2013-05-24 14:13:52 -0700

    Changed three different pictures.

--- src/edu/ucsb/cs56/S13/drawings/wolfenbarger/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/S13/drawings/wolfenbarger/advanced/AllMyDrawings.java
@@ -81,29 +81,29 @@ public class AllMyDrawings
 
 	// Draw some coffee cups.
 	
-	CoffeeCup large = new CoffeeCup(100,50,225,150);
-	CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
-	CoffeeCup tallSkinny = new CoffeeCup(20,150,20,40);
-	CoffeeCup shortFat = new CoffeeCup(20,250,40,20);
+	Caterpillar large = new Caterpillar(100,50,225,150);
+	Caterpillar smallCC = new Caterpillar(20,50,40,30);
+	Caterpillar tallSkinny = new Caterpillar(20,150,20,40);
+	Caterpillar shortFat = new Caterpillar(20,250,40,20);
 	
 	g2.setColor(Color.RED);     g2.draw(large);
 	g2.setColor(Color.GREEN);   g2.draw(smallCC);
 	g2.setColor(Color.BLUE);    g2.draw(tallSkinny);
 	g2.setColor(Color.MAGENTA); g2.draw(shortFat);
 	
-	House h1 = new House(100,250,50,75);
-	g2.setColor(Color.CYAN); g2.draw(h1);
+	Caterpillar c1 = new Caterpillar(100,250,50,75);
+	g2.setColor(Color.CYAN); g2.draw(c1);
 	
 	// Make a black house that's half the size, 
 	// and moved over 150 pixels in x direction
-	Shape h2 = ShapeTransforms.scaledCopyOfLL(h1,0.5,0.5);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
-	g2.setColor(Color.BLACK); g2.draw(h2);
+	Shape c2 = ShapeTransforms.scaledCopyOfLL(c1,0.5,0.5);
+	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
+	g2.setColor(Color.BLACK); g2.draw(c2);
 	
 	// Here's a house that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
-	h2 = ShapeTransforms.scaledCopyOfLL(h2,4,4);
-	h2 = ShapeTransforms.translatedCopyOf(h2,150,0);
+	c2 = ShapeTransforms.scaledCopyOfLL(c2,4,4);
+	c2 = ShapeTransforms.translatedCopyOf(c2,150,0);
 	
 	// We'll draw this with a thicker stroke
 	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);       
@@ -115,26 +115,26 @@ public class AllMyDrawings
 	Stroke orig=g2.getStroke();
 	g2.setStroke(thick);
 	g2.setColor(new Color(0x002FA7)); 
-	g2.draw(h2); 
+	g2.draw(c2);
 	
 	// Draw two houses with Windows
 	
-	HouseWithWindows hw1 = new HouseWithWindows(50,350,40,75);
-	HouseWithWindows hw2 = new HouseWithWindows(200,350,200,100);
+	CaterpillarWithHat ch1 = new CaterpillarWithHat(50,350,40,75);
+	CaterpillarWithHat ch2 = new CaterpillarWithHat(200,350,200,100);
 	
-	g2.draw(hw1);
+	g2.draw(ch1);
 	g2.setColor(new Color(0x8F00FF)); 
 
 	// Rotate the second house 45 degrees around its center.
-	Shape hw3 = ShapeTransforms.rotatedCopyOf(hw2, Math.PI/4.0);
+	Shape ch3 = ShapeTransforms.rotatedCopyOf(ch2, Math.PI/4.0);
 
-	g2.draw(hw3);
+	g2.draw(ch3);
 	
 	// @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	
 	g2.setStroke(orig);
 	g2.setColor(Color.BLACK); 
-	g2.drawString("A bunch of Coffee Cups and a few houses by Phill Conrad", 20,20);
+	g2.drawString("A bunch of caterpillars with and without their hats on by Martin Wolfenbarger", 20,20);
     }
   
     /** Draw a different picture with a few houses and coffee cups
@@ -144,16 +144,16 @@ public class AllMyDrawings
 	
 	// label the drawing
 	
-	g2.drawString("A bunch of Coffee Cups by Phill Conrad", 20,20);
+	g2.drawString("A bunch of caterpillars by Martin Wolfenbarger", 20,20);
 
 	
 	// Draw some coffee cups.
 	
-       CoffeeCup large = new CoffeeCup(100,50,225,150);
-       CoffeeCup smallCC = new CoffeeCup(20,50,40,30);
+       Caterpillar large = new Caterpillar(100,50,225,150);
+       CaterpillarWithHat small = new CaterpillarWithHat(20,50,40,30);
        
        g2.setColor(Color.RED);     g2.draw(large);
-       g2.setColor(Color.GREEN);   g2.draw(smallCC);
+       g2.setColor(Color.GREEN);   g2.draw(small);
        
        
     }
