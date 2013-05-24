c959d6dd1bdebbf0453f64afc99bfbc6815f1011
Brandon Newman <bnewman@umail.ucsb.edu>
2013-05-23 22:47:03 -0700

    All the drawings completed. Need to make code better to read and fix up some comments

---
+++ src/edu/ucsb/cs56/S13/drawings/bnewman/advanced/AllMyDrawings.java
@@ -0,0 +1,105 @@
+package edu.ucsb.cs56.S13.drawings.bnewman.advanced;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;  // single lines
+import java.awt.geom.Ellipse2D;  // ellipses and circles
+import java.awt.geom.Rectangle2D; // for the bounding box
+import java.awt.Rectangle;  // squares and rectangles
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+import java.awt.Color; // class for Colors
+import java.awt.Stroke;
+import java.awt.BasicStroke;
+
+
+import edu.ucsb.cs56.S13.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
+
+/**
+ * A class with static methods for drawing various pictures
+ * 
+ * @author Phill Conrad 
+ * @version for CS10, lab06, Spring 2009
+ */
+
+
+public class AllMyDrawings
+{
+    /** Draw a picture with a few houses 
+     */
+
+    public static void drawPicture1(Graphics2D g2) {
+
+	RobotFace red  = new RobotFace(100,150,50);
+	RobotFaceWithAnt green = new RobotFaceWithAnt(200,150,50);
+	RobotFace blue  = new RobotFace(300,150,50);
+	RobotFaceWithAnt magenta = new RobotFaceWithAnt(400,150,50);
+       
+	g2.setColor(Color.GREEN); g2.draw(green);
+	g2.setColor(Color.RED); g2.draw(red);
+	g2.setColor(Color.BLUE); g2.draw(blue);
+	g2.setColor(new Color(0x790EAD)); g2.draw(magenta);
+
+	g2.setColor(Color.BLACK);
+	g2.drawString("A rag tag team of multicolored robot faces", 20,20);
+	
+    }
+
+
+    /** Draw a picture with a few houses and coffee cups
+     */
+    public static void drawPicture2(Graphics2D g2) {
+
+	RobotFace red  = new RobotFace(200,200,70);
+	RobotFaceWithAnt green = new RobotFaceWithAnt(300,150,70);
+	RobotFace blue  = new RobotFace(400,200,70);
+	RobotFaceWithAnt purple = new RobotFaceWithAnt(300,250,70);
+	
+	Shape rotatedRed = ShapeTransforms.rotatedCopyOf(red, Math.PI/2.0);
+	Shape rotatedBlue = ShapeTransforms.rotatedCopyOf(blue, -1*(Math.PI/2.0));
+        Shape rotatedGreen = ShapeTransforms.rotatedCopyOf(green, Math.PI);
+
+	g2.setColor(Color.RED); g2.draw(rotatedRed);
+	g2.setColor(Color.BLUE); g2.draw(rotatedBlue);
+        g2.setColor(Color.GREEN); g2.draw(rotatedGreen);
+        g2.setColor(new Color(0x790EAD)); g2.draw(purple);
+
+	g2.setColor(Color.BLACK); 
+	g2.drawString("However these robot faces mostly just gaze at clouds all day", 20,20);
+    }
+  
+    /** Draw a different picture with a few houses and coffee cups
+     */
+
+    public static void drawPicture3(Graphics2D g2) {
+	
+	// label the drawing
+	g2.drawString("And, of course, you don't have a robot face without a robot face parent", 20,20);
+
+	
+	// Draw some coffee cups.
+	RobotFaceWithAnt parent = new RobotFaceWithAnt(400, 300,50);
+	RobotFace baby = new RobotFace(400, 270, 50);
+	    
+	Shape bigParent = ShapeTransforms.scaledCopyOfLL(parent,4,4);
+	Shape finalParent = ShapeTransforms.translatedCopyOf(bigParent,-300,0);
+
+	g2.setColor(Color.BLUE); g2.draw(baby);
+
+	Stroke thick = new BasicStroke (4.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL);
+	g2.setStroke(thick);
+	g2.setColor(Color.RED); g2.draw(finalParent);
+       
+       
+    }
+
+    public static void drawPicture4(Graphics2D g2) {
+
+	RobotFace large = new RobotFace(100,100,200);
+	RobotFaceWithAnt rob = new RobotFaceWithAnt(100,100,200);
+	g2.setColor(Color.GREEN); g2.draw(rob);
+    }
+    
+
+}
---
+++ src/edu/ucsb/cs56/S13/drawings/bnewman/advanced/RobotFace.java
@@ -0,0 +1,78 @@
+package edu.ucsb.cs56.S13.drawings.bnewman.advanced;
+import java.awt.geom.GeneralPath; // combinations of lines and curves
+import java.awt.geom.AffineTransform; // translation, rotation, scale
+import java.awt.Shape; // general class for shapes
+
+// all imports below this line needed if you are implementing Shape
+import java.awt.geom.Point2D; 
+import java.awt.geom.Line2D; 
+import java.awt.geom.Rectangle2D;
+import java.awt.Rectangle;
+import java.awt.geom.PathIterator;
+import java.awt.geom.AffineTransform;
+
+import edu.ucsb.cs56.S13.drawings.utilities.ShapeTransforms;
+import edu.ucsb.cs56.S13.drawings.utilities.GeneralPathWrapper;
+
+/**
+   A vector drawing of a robot's face that implements
+   the Shape interface, and so can be drawn, as well as
+   rotated, scaled, etc.
+      
+   @author Brandon Newman
+   @version for CS56, Spring 2013, UCSB
+   
+*/
+public class RobotFace extends GeneralPathWrapper implements Shape
+{
+    /**
+       Constructor
+
+       @param x x coord of lower left corner of house
+       @param y y coord of lower left corner of house
+       @param length length of side of the face
+     */
+    public RobotFace(double x, double y, double length)
+    {
+    
+        // Rather than having to scale at the end, we can just
+        // draw things the right way to begin with, using the
+        // x, y, width and height.   If you haven't already
+        // hard coded a particular drawing, this may be an easier
+        // way.
+
+
+        
+        // Make the head 
+	Rectangle2D.Double head = 
+	    new Rectangle2D.Double(x, y, length, length);
+
+
+	//Make the eyes
+	Rectangle2D.Double eyeballLeft =
+	    new Rectangle2D.Double(x + length/8, y + length/8, length/4, length/4);
+
+	Rectangle2D.Double eyeballRight =
+	    new Rectangle2D.Double(x + 5*length/8, y + length/8, length/4, length/4);
+
+
+	//Make the mouth
+	Rectangle2D.Double mouth =
+	   new Rectangle2D.Double(x + length/8, y + 5*length/8, 3*length/4, length/4);
+
+	//Make the cool screen thingy
+	Line2D.Double screen = 
+	    new Line2D.Double(x + length/8, y + 3*length/4, x + 7*length/8, y + 3*length/4);
+
+	//Put it all together
+	GeneralPath entireRobotFace = this.get();
+	entireRobotFace.append(head, false);
+	entireRobotFace.append(eyeballLeft, false);
+       	entireRobotFace.append(eyeballRight, false);
+	entireRobotFace.append(mouth, false);
+	entireRobotFace.append(screen, false);
+
+           
+    }
+
+}
